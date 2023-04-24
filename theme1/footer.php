997a548e881ca34ccd4b8f5577568bcaaddc8be9
aspirati0 <99715792+aspirati0@users.noreply.github.com>
2023-04-24 18:35:39 +0300

    Add files via upload

---
+++ theme1/footer.php
@@ -0,0 +1,45 @@
+    <div class="footer" style="display: flex; grid-gap: 70px; justify-content: center; background: #EAEAEA;">
+        <div style="width: 269px">
+            <img src="/wp-content/themes/theme1/assets/img/logo1.png" style="width: 110px; height: 40px" />
+            <p style="font-family: 'Lora'; font-style: normal; font-weight: 400; font-size: 14px; line-height: 20px; color: #777777; ">Condimentum adipiscing vel neque dis nam parturient orci at scelerisque neque dis nam parturient.</p>
+            <ul style="list-style: none; padding-left: 0; ">
+                <li style="grid-gap: 5px; font-family: 'Lora'; font-style: normal; font-weight: 400; font-size: 14px; line-height: 20px; color: #777777; display: flex; align-items: center;"><img src="/wp-content/themes/theme1/assets/img/li1.png" />451 Wall Street, UK, London</li>
+                <li style="grid-gap: 5px; font-family: 'Lora'; font-style: normal; font-weight: 400; font-size: 14px; line-height: 20px; color: #777777; display: flex; align-items: center;"><img src="/wp-content/themes/theme1/assets/img/li2.png" />Phone: (064) 332-1233</li>
+                <li style="grid-gap: 5px; font-family: 'Lora'; font-style: normal; font-weight: 400; font-size: 14px; line-height: 20px; color: #777777; display: flex; align-items: center;"><img src="/wp-content/themes/theme1/assets/img/li3.png" />Fax: (099) 453-1357</li>
+            </ul>
+        </div>
+
+        <div style="">
+            <h3 style="font-family: 'Marcellus'; font-style: normal; font-weight: 400; font-size: 18px; line-height: 25px; text-transform: uppercase; color: #000000; ">RECENT POSTS</h3>
+            <div>
+                <div style="display: flex; grid-gap: 15px; border-bottom: #dfdfdf solid 1px; padding-bottom: 10px; margin-bottom: 10px"><img src="/wp-content/themes/theme1/assets/img/footer.png" style="width: 75px; height: 65px;" /><div><p style="font-family: 'Marcellus'; font-style: normal; font-weight: 400; font-size: 14px; line-height: 20px; text-transform: uppercase; color: #242424; margin: 0">A companion for extra sleeping</p><p style="font-family: 'Lora'; font-style: normal; font-weight: 400; font-size: 14px; line-height: 14px; color: #777777; margin-top: 8px">July 23, 2016</p></div></div>
+            </div>
+        </div>
+
+        <div class="links">
+            <h3 style="font-family: 'Marcellus'; font-style: normal; font-weight: 400; font-size: 18px; padding-right: 20px; line-height: 25px; text-transform: uppercase; color: #000000; ">OUR STORES</h3>
+            <ul style="list-style: none; padding-left: 0">
+                <li>New York</li>
+                <li>London SF</li>
+                <li>Cockfosters BP</li>
+                <li>Los Angeles</li>
+                <li>Chicago</li>
+            </ul>
+        </div>
+
+        <div class="links">
+            <h3 style="font-family: 'Marcellus'; font-style: normal; font-weight: 400; font-size: 18px; line-height: 25px; text-transform: uppercase; color: #000000; ">USEFUL LINKS</h3>
+            <ul style="list-style: none; padding-left: 0">
+                <li>Privacy Policy</li>
+                <li>Returns</li>
+                <li>Terms & Conditions</li>
+                <li>Contact Us</li>
+                <li>Latest News</li>
+            </ul>
+        </div>
+
+    </div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
