eb109cb398892ab7bb2c252581d6baa330ea2060
aspirati0 <99715792+aspirati0@users.noreply.github.com>
2023-04-26 17:56:25 +0300

    Added the ability for admins to customize the theme from the admin panel

---
+++ header.php
@@ -0,0 +1,23 @@
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css" />
+    <?php wp_head(); ?>
+</head>
+<body>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+
+    <header>
+        <?php the_custom_logo(); ?>
+        <?php wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
+        <p class="login">LOGIN / REGISTER</p>
+        <div>
+            <img class="search" src="/wp-content/themes/theme1/assets/img/search.png" />
+            <div>
+                <img class="cart" src="/wp-content/themes/theme1/assets/img/cart.png" />
+                <p>$0.00</p>
+            </div>
+        </div>
+    </header>
